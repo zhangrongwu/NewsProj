99f6b6cbcf469f6221ce06cd9a4b3d21f86af61d
zhangrongwu <zhangrongwuios@icloud.com>
2018-08-14 18:11:03 +0800

    刷新添加

--- src/components/moreMovieList.vue
+++ src/components/moreMovieList.vue
@@ -1,18 +1,17 @@
 <template>
     <div class="more">
-        <!-- sss {{items.title}} -->
-        <van-pull-rfresh v-model="isLoading" @refresh="onRefresh">
-            <van-list v-model="loadMore" :finished="finished" @load="onLoadMore">
+        <van-pull-refresh v-model="refreshing" @refresh="onRefresh" pulling-text="下拉即可刷新..." loosing-text="释放即可刷新..." loading-text="加载中...">
+            <van-list v-model="loadMore" :finished="finished" @load="onLoadMore" loading-text="加载中..." :offset="offset">
                 <van-cell v-for="(item, index) in items" :key="index">
                     <div class="info_wrap">
                         <span class="title">{{item.title}}({{item.original_title}})</span>
-                        <span class="info-wrap-type">{{item.genres | getMovieType}}</span>
+                        <span class="info-wrap-type">{{item.genres}}</span>
                         <span class="info-wrap-year">上映时间:{{item.year}}</span>
                         <span class="info-warp-author">作者简介</span>
                     </div>
                 </van-cell>
             </van-list>
-        </van-pull-rfresh>
+        </van-pull-refresh>
     </div>
 </template>
 
@@ -21,9 +20,10 @@ export default {
     props: ["items"],
     data() {
         return {
-            isLoading: false,
+            refreshing: false,
             loadMore: false,
-            finished: false
+            finished: false,
+            offset: 300
         };
     },
     created() {
@@ -31,9 +31,10 @@ export default {
     },
     methods: {
         onRefresh() {
+            debugger;
             console.log("下拉刷新");
             setTimeout(() => {
-                this.isLoading = false;
+                this.refreshing = false;
             }, 500);
         },
         onLoadMore() {
@@ -53,7 +54,7 @@ export default {
 <style scoped>
 .more {
     width: 100%;
-    height: 100%;
+    height: 80%;
 }
 .info_wrap {
     display: flex;
