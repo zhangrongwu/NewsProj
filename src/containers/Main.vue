c9d5be724a37a9a56759b7d7ba5744c8cdc0d369
zhangrongwu <zhangrongwuios@icloud.com>
2018-09-07 15:51:55 +0800

    关闭

--- src/containers/Main.vue
+++ src/containers/Main.vue
@@ -94,8 +94,8 @@ export default {
 }
 
 .middle {
-    width: 100%;
-    height: 100%;
+    /* width: 100%;
+    height: 100%; */
     /* align-items: center;
     justify-content: center;
     position: absolute; */
